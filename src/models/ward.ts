3496b6561bbd26245d9929d40dc454faf5e589b0
ignitiusgohyz <ignitiusgohyz@gmail.com>
2023-09-20 12:47:59 +0800

    fix: ward model

--- src/models/ward.ts
+++ src/models/ward.ts
@@ -2,14 +2,14 @@ import { SmartBed } from "./smartBed";
 
 export class Ward {
   _id: string;
-  wardNum: string;
+  num: string;
   wardType: string;
   numRooms: number;
   smartBeds?: SmartBed[];
 
   constructor(_id: string, num: string, wardType: string, numRooms: number, smartBeds?: SmartBed[]) {
     this._id = _id;
-    this.wardNum = num;
+    this.num = num;
     this.wardType = wardType;
     this.numRooms = numRooms;
     this.smartBeds = smartBeds;
--- src/pages/wards.tsx
+++ src/pages/wards.tsx
@@ -12,6 +12,7 @@ export default function Wards() {
 
   useEffect(() => {
     fetchAllWards().then((res) => setWardsList(res.data));
+    console.log(wardsList);
   }, [wardsList.length]);
 
   const viewWardVisualisation = (wardId: string, wardNum: string) => {
@@ -35,14 +36,14 @@ export default function Wards() {
           <Grid item xs={12} sm={6} md={4} lg={3} key={ward._id}>
             <Paper
               sx={{ ":hover": { cursor: "pointer" } }}
-              onClick={() => viewWardVisualisation(ward._id, ward.wardNum)}
+              onClick={() => viewWardVisualisation(ward._id, ward.num)}
               elevation={3}
               style={{
                 padding: "16px",
               }}
             >
               <Typography variant="h6">
-                Ward: {ward.wardNum}, Bed Count : {ward.smartBeds?.length}
+                Ward: {ward.num}, Bed Count : {ward.smartBeds?.length}
               </Typography>
             </Paper>
           </Grid>
