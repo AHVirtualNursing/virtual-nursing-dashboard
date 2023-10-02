631928a4f77056c6ddaff50e844ff201d69200dd
wglee99 <littlereddot40@gmail.com>
2023-10-02 17:43:02 +0800

    style: change colors of bars in dashboard

--- src/components/PendingFollowUps.tsx
+++ src/components/PendingFollowUps.tsx
@@ -2,52 +2,76 @@ import React from "react";
 import {
   BarChart,
   Bar,
+  Tooltip,
   Legend,
   ResponsiveContainer,
   XAxis,
   YAxis,
+  Label,
+  LabelList,
 } from "recharts";
 
 const data = [
-  {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
   {
     name: "Page B",
     uv: 3000,
     pv: 1398,
     amt: 2210,
   },
-  {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
 ];
 
 export default function PendingFollowUps() {
   return (
     <div className="flex flex-col w-1/2 p-4 gap-y-3">
-      <h3 className="text-left">Patients</h3>
-      <div className="h-full flex gap-x-5 justify-start">
-        <div className="w-full">
+      <h3 className="text-left">Pending Follow Ups</h3>
+      <div className="h-full flex gap-x-5">
+        <div className="w-1/4 rounded-md bg-pink-200 flex items-center justify-center p-3">
+          <p>Total: 140</p>
+        </div>
+        <div className="w-3/4">
           <ResponsiveContainer width="80%" height="100%">
-            <BarChart width={150} height={40} data={data} barCategoryGap="10%">
-              <Legend
-                layout="vertical"
-                verticalAlign="middle"
-                align="right"
-                wrapperStyle={{
-                  paddingLeft: "30px",
+            <BarChart
+              width={150}
+              height={40}
+              data={data}
+              maxBarSize={30}
+              layout="vertical"
+            >
+              <XAxis type="number" hide />
+              <YAxis dataKey="name" type="category" hide />
+              <Bar
+                dataKey="uv"
+                fill="#7F94DA"
+                background={{
+                  radius: 20,
+                  height: 15,
                 }}
-              />
-              <XAxis dataKey="name" type="category" hide />
-              <YAxis type="number" hide />
-              <Bar dataKey="uv" fill="#8884d8" radius={10} />
+                radius={[10, 10, 10, 10]}
+              >
+                <LabelList dataKey={"uv"} position={"center"} fill="black" />
+              </Bar>
+              <Bar
+                dataKey="pv"
+                fill="#82ca9d"
+                background={{
+                  radius: 20,
+                  height: 15,
+                }}
+                radius={[10, 10, 10, 10]}
+              >
+                <LabelList dataKey={"pv"} position={"center"} fill="black" />
+              </Bar>
+              <Bar
+                dataKey="amt"
+                fill="#ffc658"
+                background={{
+                  radius: 20,
+                  height: 15,
+                }}
+                radius={[10, 10, 10, 10]}
+              >
+                <LabelList dataKey={"amt"} position={"center"} fill="black" />
+              </Bar>
             </BarChart>
           </ResponsiveContainer>
         </div>
--- src/components/summary.tsx
+++ src/components/summary.tsx
@@ -2,62 +2,49 @@ import React from "react";
 import {
   BarChart,
   Bar,
-  Tooltip,
   Legend,
   ResponsiveContainer,
   XAxis,
   YAxis,
+  LabelList,
 } from "recharts";
 
 const data = [
-  {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
   {
     name: "Page B",
     uv: 3000,
     pv: 1398,
     amt: 2210,
   },
-  {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
 ];
 
 export default function Summary() {
   return (
     <div className="flex flex-col w-1/2 p-4 gap-y-3">
-      <h3 className="text-left">Summary</h3>
-      <div className="h-full flex gap-x-5">
-        <div className="w-1/4 rounded-md bg-pink-200 flex items-center justify-center p-3">
-          <p>Total: 140</p>
-        </div>
-        <div className="w-3/4">
+      <h3 className="text-left">Patients Summary</h3>
+      <div className="h-full flex gap-x-5 justify-start">
+        <div className="w-full">
           <ResponsiveContainer width="80%" height="100%">
-            <BarChart
-              width={150}
-              height={40}
-              data={data}
-              maxBarSize={30}
-              layout="vertical"
-            >
-              <XAxis type="number" hide />
-              <YAxis dataKey="name" type="category" hide />
-              <Bar
-                dataKey="uv"
-                fill="#8884d8"
-                background={{
-                  radius: 20,
-                  height: 15,
+            <BarChart width={150} height={40} data={data} barCategoryGap="10%">
+              <Legend
+                layout="vertical"
+                verticalAlign="middle"
+                align="right"
+                wrapperStyle={{
+                  paddingLeft: "30px",
                 }}
-                radius={[10, 10, 10, 10]}
               />
+              <XAxis dataKey="name" type="category" hide />
+              <YAxis type="number" hide />
+              <Bar dataKey="uv" fill="#8884d8" radius={10}>
+                <LabelList dataKey={"uv"} position={"center"} fill="black" />
+              </Bar>
+              <Bar dataKey="pv" fill="#ffc658" radius={10}>
+                <LabelList dataKey={"pv"} position={"center"} fill="black" />
+              </Bar>
+              <Bar dataKey="amt" fill="#82ca9d" radius={10}>
+                <LabelList dataKey={"amt"} position={"center"} fill="black" />
+              </Bar>
             </BarChart>
           </ResponsiveContainer>
         </div>
