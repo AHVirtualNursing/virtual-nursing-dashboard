5680dcbf29d14491a881fcd22b1196520fdfe1e4
wglee99 <littlereddot40@gmail.com>
2023-10-07 11:13:00 +0800

    style: sidebar responsiveness, dashboard spacing

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -8,7 +8,7 @@ import Link from "next/link";
 
 export default function Sidebar() {
   return (
-    <div className="left-0 flex flex-col h-[calc(100vh-78px)] pt-9 shadow-lg gap-4 w-1/6">
+    <div className="left-0 flex flex-col h-[calc(100vh-78px)] pt-9 shadow-lg gap-4 w-48">
       <SidebarTab name={"Patients"} icon={<GroupIcon />} dest={"dashboard"} />
       <SidebarTab name={"Wards"} icon={<BedIcon />} dest={"wards"} />
       <SidebarTab
@@ -36,7 +36,7 @@ const SidebarTab = ({ name, icon, dest }: SidebarTabProps) => {
     <Link href={dest}>
       <div className="flex group gap-3 hover:bg-slate-200 cursor-pointer p-4 rounded-md">
         {icon}
-        <span className="text-md">{name}</span>
+        <span className="text-md max-sm:hidden">{name}</span>
       </div>
     </Link>
   );
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -4,13 +4,13 @@ import Summary from "@/components/Summary";
 
 export default function Dashboard() {
   return (
-    <div className="flex flex-col p-3 gap-4 bg-blue-100 w-full shadow-lg">
+    <div className="flex flex-col p-8 gap-8 bg-blue-100 w-full shadow-lg">
       <h1>Virtual Nurse Dashboard</h1>
-      <div className="bg-white rounded-lg h-2/6 pt-7 flex shadow-lg ">
+      <div className="bg-white rounded-2xl h-2/6 p-4 flex shadow-lg ">
         <PendingFollowUps />
         <Summary />
       </div>
-      <div className="bg-white rounded-lg h-4/6 p-3 shadow-lg">
+      <div className="bg-white rounded-2xl h-4/6 p-3 shadow-lg">
         <Patients />
       </div>
     </div>
