d96b2d551d9fdf51fd4c6ab8194cd79a640deffe
wglee99 <littlereddot40@gmail.com>
2023-10-16 23:30:50 +0800

    feat: only patient name routes to patient dashboard on click

--- src/pages/configs.tsx
+++ src/pages/configs.tsx
@@ -1,5 +1,15 @@
+import PendingFollowUps from "@/components/PendingFollowUps";
 import React from "react";
 
-export default function Configs() {
-  return <div>This is the conflict page</div>;
+function Configs() {
+  return (
+    <div className="flex flex-col p-8 gap-8 bg-blue-100 w-full shadow-lg">
+      <h4 className="flex flex-start">Conditions</h4>
+      <div className="bg-white rounded-2xl h-1/2 p-4 flex shadow-lg "></div>
+      <h4 className="flex flex-start">Actions</h4>
+      <div className="bg-white rounded-2xl h-1/2 p-3 shadow-lg"></div>
+    </div>
+  );
 }
+
+export default Configs;
