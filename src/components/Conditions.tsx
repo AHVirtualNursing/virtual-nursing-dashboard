fa831b2bf238fd3c8a1fb5914d5e2f313b081dbf
wglee99 <littlereddot40@gmail.com>
2023-10-23 18:43:20 +0800

    feat: render list of conditions in droppable div

--- src/components/Conditions.tsx
+++ src/components/Conditions.tsx
@@ -1,17 +1,25 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import DeleteIcon from '@mui/icons-material/Delete';
 
 type ConditionsProps = {
   conditionsList: string[];
 }
 
 function Conditions({conditionsList}: ConditionsProps) {
+  const [conditions, setConditions] = useState<string[]>(conditionsList);
+
+  function handleDelete(index: number) {
+    
+  }
+
   return (
-    <ul>
-      {
-        conditionsList.map((item, index) => (
-          <div key={index} className="bg-amber-100 p-2 rounded-lg shadow-lg">{item}</div>
-        ))
-      }
+    <ul className="space-y-2">
+      {conditions.map((item, index) => (
+        <div key={index} className="bg-amber-100 p-2 rounded-lg shadow-lg flex justify-between">
+          {item}
+          <DeleteIcon fontSize={'small'} onClick={() => handleDelete(index)}/>
+        </div>
+      ))}
     </ul>
   )
 }
