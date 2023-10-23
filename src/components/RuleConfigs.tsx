cb92c75c53b7c4fb4bc10038e3dfe3574b824212
wglee99 <littlereddot40@gmail.com>
2023-10-23 16:51:13 +0800

    refactor: use react's native drag and drop functionality

--- src/components/RuleConfigs.tsx
+++ src/components/RuleConfigs.tsx
@@ -1,69 +1,54 @@
-import {closestCenter, closestCorners, DndContext, DragEndEvent, DragOverEvent} from "@dnd-kit/core";
 import React from "react";
-import ConditionsDroppable from "@/components/ConditionsDroppable";
-import ConditionDraggable from "@/components/ConditionDraggable";
+import Conditions from "@/components/Conditions";
 
 function RuleConfigs() {
   const conditions = [
     {title: "Measurement result"},
     {title: "Measurement % deviation"},
     {title: "Measurement by time of day"},
-    {title: "Survey result level"},
-    {title: "Survey result score"},
-    {title: "Survey result score"},
-    {title: "Survey result score"},
-    {title: "Survey result score"},
-    {title: "Survey result score"},
-    {title: "Survey result score"},
-    {title: "Survey result score"},
-    {title: "Survey result score"},
-    {title: "Survey result score"},
-
-
   ];
   const [allConditions, setAllConditions] = React.useState<string[]>([])
 
-  const addConditionsToRules = (event: DragEndEvent) => {
-    console.log(event)
-    const newCondition = event.active.data.current?.title;
-    if (event.over) {
-      const temp = [...allConditions];
-      temp.push(newCondition);
-      setAllConditions(temp);
-    }
+  function handleOnDrag(event: React.DragEvent, title: string) {
+    event.dataTransfer.setData("title", title);
+  }
+
+  function handleDropOver(event: React.DragEvent) {
+    event.preventDefault();
+  }
+
+  function handleDrop(event: React.DragEvent) {
+    const title = event.dataTransfer.getData('title');
+    setAllConditions([...allConditions, title]);
   }
 
   return (
     <div className="flex flex-col h-full gap-y-1">
       <h4 className="flex flex-start">Conditions</h4>
-      <DndContext onDragEnd={addConditionsToRules}>
-        <div
-          id="rules-conditions"
-          className="bg-white rounded-2xl h-1/2 flex shadow-lg "
-        >
-          <div className="w-1/4 text-center pt-2 flex flex-col">
-            <h4 className={"p-2"}>
-              Measurements
-            </h4>
-            <div className="bg-slate-200 text-center h-full overflow-auto scrollbar">
-              <ul>
-                {conditions.map((condition, index) => (
-                  <ConditionDraggable key={condition.title}>
-                    <div key={index} className="rounded-lg p-1">
-                      {condition.title}
-                    </div>
-                  </ConditionDraggable>
-                ))}
-              </ul>
-            </div>
-
-          </div>
-
-          <div className="w-3/4">
-            <ConditionsDroppable conditions={allConditions}/>
+      <div
+        id="rules-conditions"
+        className="bg-white rounded-2xl h-1/2 flex shadow-lg "
+      >
+        <div className="w-1/4 text-center pt-2 flex flex-col">
+          <h4 className={"p-2"}>
+            Measurements
+          </h4>
+          <div className="bg-slate-200 text-center h-full overflow-y-scroll scrollbar">
+            <ul>
+              {conditions.map((condition, index) => (
+                <div key={index} className="rounded-lg p-1 hover:cursor-grab" draggable
+                     onDragStart={(e) => handleOnDrag(e, condition.title)}>
+                  {condition.title}
+                </div>
+              ))}
+            </ul>
           </div>
         </div>
-      </DndContext>
+
+        <div className="w-3/4 p-2" onDragOver={handleDropOver} onDrop={handleDrop}>
+          <Conditions conditionsList={allConditions}/>
+        </div>
+      </div>
       <h4 className="flex flex-start">Actions</h4>
       <div
         id="rules-actions"
