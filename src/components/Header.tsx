025023a05aacc65ce96709aa79c47e7d7cd0714d
wglee99 <littlereddot40@gmail.com>
2023-10-24 22:54:17 +0800

    fix: change welcome message font

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,19 @@
 import Image from "next/image";
 import styles from "@/styles/Header.module.css";
-import {useRouter} from "next/navigation";
-import {signOut, useSession} from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { signOut, useSession } from "next-auth/react";
 import LogoutIcon from "@mui/icons-material/Logout";
 import ChangePasswordModal from "./ChangePasswordModal";
-import {useEffect, useState} from "react";
-import {VirtualNurse} from "@/models/virtualNurse";
-import {fetchVirtualNurseByNurseId} from "@/pages/api/nurse_api";
+import { useEffect, useState } from "react";
+import { VirtualNurse } from "@/models/virtualNurse";
+import { fetchVirtualNurseByNurseId } from "@/pages/api/nurse_api";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Header() {
   const router = useRouter();
-  const {data: sessionData} = useSession();
+  const { data: sessionData } = useSession();
   const [nurse, setNurse] = useState<VirtualNurse>();
 
   useEffect(() => {
@@ -20,8 +23,7 @@ export default function Header() {
   }, [sessionData?.user.id]);
 
   return (
-    <div
-      className="flex align-middle justify-between p-2 border-solid border-0 border-b-2 border-gray-300">
+    <div className="flex align-middle justify-between p-2 border-solid border-0 border-b-2 border-gray-300">
       <div className={styles.imageContainer}>
         <Image
           src={"/VND_Logo.png"}
@@ -32,10 +34,10 @@ export default function Header() {
         />
       </div>
       <div className="flex gap-5 items-center">
-        <span>Welcome {nurse?.name}</span>
-        <ChangePasswordModal/>
+        <span className={`${inter.className}`}>Welcome, {nurse?.name}</span>
+        <ChangePasswordModal />
         <button className="bg-transparent border-none">
-          <LogoutIcon titleAccess="Logout" onClick={() => signOut()}/>
+          <LogoutIcon titleAccess="Logout" onClick={() => signOut()} />
         </button>
       </div>
     </div>
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,10 +1,11 @@
 import PendingFollowUps from "@/components/PendingFollowUps";
 import Patients from "@/components/Patients";
 import Summary from "@/components/Summary";
-import {useRouter} from "next/navigation";
+import { useRouter } from "next/navigation";
 
 export default function Dashboard() {
   const router = useRouter();
+
   return (
     <div className="flex flex-col p-8 gap-8 w-full shadow-lg bg-slate-100">
       <div className="flex justify-between">
@@ -17,11 +18,11 @@ export default function Dashboard() {
         </button>
       </div>
       <div className="bg-white rounded-2xl h-2/6 p-4 flex shadow-lg ">
-        <PendingFollowUps/>
-        <Summary/>
+        <PendingFollowUps />
+        <Summary />
       </div>
       <div className="bg-white rounded-2xl h-4/6 p-3 shadow-lg">
-        <Patients/>
+        <Patients />
       </div>
     </div>
   );
