1407425afb35d00bb00a6a71c26d004040caa3e9
gwajoon <hengguojun25@gmail.com>
2023-10-25 14:37:50 +0800

    Change threshold annotations to box

--- src/components/patientAnalyticsChart/patientAnalyticsChartOptions.tsx
+++ src/components/patientAnalyticsChart/patientAnalyticsChartOptions.tsx
@@ -9,36 +9,27 @@ export const showNormalRangeAnnotations = (selectedVitals: {
 
   if (selectedVitals.heartRate) {
     normalRangeAnnotations.heartRateMin = {
-      type: "line",
+      type: "box",
       yMin: 60,
-      yMax: 60,
-      borderColor: "rgb(255, 191, 0)",
-      borderWidth: 2,
+      yMax: 100,
+      backgroundColor: "rgb(255, 191, 0, 0.25)",
+      borderWidth: 0,
     };
 
     normalRangeAnnotations.heartRateMinLabel = {
       type: "label",
-      xValue: 5,
-      yValue: 61,
-      color: "rgb(255, 191, 0)",
+      yValue: 59,
+      color: "rgb(255, 162, 0)",
       content: ["Min Heart Rate (bpm)"],
       font: {
         size: 12,
       },
     };
 
-    normalRangeAnnotations.heartRateMax = {
-      type: "line",
-      yMin: 100,
-      yMax: 100,
-      borderColor: "rgb(255, 191, 0)",
-      borderWidth: 2,
-    };
-
     normalRangeAnnotations.heartRateMaxLabel = {
       type: "label",
       yValue: 101,
-      color: "rgb(255, 191, 0)",
+      color: "rgb(255, 162, 0)",
       content: ["Max Heart Rate (bpm)"],
       font: {
         size: 12,
@@ -48,17 +39,17 @@ export const showNormalRangeAnnotations = (selectedVitals: {
 
   if (selectedVitals.spO2) {
     normalRangeAnnotations.spO2Min = {
-      type: "line",
+      type: "box",
       yMin: 95,
-      yMax: 95,
-      borderColor: "rgb(64, 224, 208)",
-      borderWidth: 2,
+      yMax: 100,
+      backgroundColor: "rgb(64, 224, 208, 0.25)",
+      borderWidth: 0,
     };
 
     normalRangeAnnotations.spO2MinLabel = {
       type: "label",
-      yValue: 96,
-      color: "rgb(64, 224, 208)",
+      yValue: 94,
+      color: "rgb(64, 200, 224)",
       content: ["Min Blood Oxygen (%)"],
       font: {
         size: 12,
@@ -68,29 +59,21 @@ export const showNormalRangeAnnotations = (selectedVitals: {
 
   if (selectedVitals.bloodPressure) {
     normalRangeAnnotations.bloodPressureSysMin = {
-      type: "line",
+      type: "box",
       yMin: 90,
-      yMax: 90,
-      borderColor: "rgb(210, 4, 45)",
-      borderWidth: 2,
+      yMax: 120,
+      backgroundColor: "rgb(210, 4, 45, 0.25)",
+      borderWidth: 0,
     };
 
     normalRangeAnnotations.bloodPressureSysMinLabel = {
       type: "label",
-      yValue: 92,
+      yValue: 89,
       color: "rgb(210, 4, 45)",
       content: ["Min Systolic Blood Pressure (mm Hg)"],
       font: {
         size: 12,
       },
-    };
-
-    normalRangeAnnotations.bloodPressureSysMax = {
-      type: "line",
-      yMin: 120,
-      yMax: 120,
-      borderColor: "rgb(210, 4, 45)",
-      borderWidth: 2,
     };
 
     normalRangeAnnotations.bloodPressureSysMaxLabel = {
@@ -104,30 +87,21 @@ export const showNormalRangeAnnotations = (selectedVitals: {
     };
 
     normalRangeAnnotations.bloodPressureDiaMin = {
-      type: "line",
+      type: "box",
       yMin: 60,
-      yMax: 60,
-      borderColor: "rgb(255, 117, 24)",
-      borderWidth: 2,
+      yMax: 80,
+      backgroundColor: "rgb(255, 117, 24, 0.25)",
+      borderWidth: 0,
     };
 
     normalRangeAnnotations.bloodPressureDiaMinLabel = {
       type: "label",
-      xValue: 3,
-      yValue: 61,
+      yValue: 59,
       content: ["Min Diastolic Blood Pressure (mm Hg)"],
       color: "rgb(255, 117, 24)",
       font: {
         size: 12,
       },
-    };
-
-    normalRangeAnnotations.bloodPressureDiaMax = {
-      type: "line",
-      yMin: 80,
-      yMax: 80,
-      borderColor: "rgb(255, 117, 24)",
-      borderWidth: 2,
     };
 
     normalRangeAnnotations.bloodPressureDiaMaxLabel = {
