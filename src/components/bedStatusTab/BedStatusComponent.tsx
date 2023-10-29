de86d9fcc692075eeb89b3acff181958d28fe1b2
wglee99 <littlereddot40@gmail.com>
2023-10-29 12:12:17 +0800

    feat:  bed status tab to show bed information

--- src/components/bedStatusTab/BedStatusComponent.tsx
+++ src/components/bedStatusTab/BedStatusComponent.tsx
@@ -7,27 +7,29 @@ interface BedProp {
 
 function BedStatusComponent(bedProp: BedProp) {
   console.log(bedProp.bed);
-  console.log(bedProp.bed?.railStatus);
   return (
-    <div>
-      <p>check the rail status left upper lower right upper lower</p>
-      {bedProp.bed?.railStatus ? (
-        <div>Bed rails are up</div>
-      ) : (
-        <div>Bed rails are down</div>
-      )}
-      {bedProp.bed?.isBrakeSet ? (
-        <div>Bed brakes are set</div>
-      ) : (
-        <div>Bed brakes are not set</div>
-      )}
-      {bedProp.bed?.bedAlarmTriggered ? (
-        <div>Bed alarm has been triggered</div>
-      ) : (
-        <div>Bed alarm has not been triggered</div>
-      )}
-      <p>Bed position: {bedProp.bed?.bedPosition}</p>
-    </div>
+    <>
+      <div id="bed-photo">
+        <img
+          src="https://healthjade.net/wp-content/uploads/2020/02/Low-Fowler%E2%80%99s-position.jpg"
+          alt="Patient on bed raised 15-30 degrees"
+          className="object-contain w-96 h-48"
+        />
+      </div>
+      <div id="bedrail" className="rounded-lg">
+        <p>Bed Rails: {bedProp.bed?.railStatus ? "Up" : "Down"}</p>
+      </div>
+      <div id="bed-brakes">
+        <p>Bed Brakes: {bedProp.bed?.isBrakeSet ? "Set" : "Not set"}</p>
+      </div>
+      <div id="bed-alarm">
+        <p>
+          Bed Alarm:{" "}
+          {bedProp.bed?.bedAlarmTriggered ? "Triggered" : "Not triggered"}
+        </p>
+      </div>
+      <div id="bed-position">Bed Position is {bedProp.bed?.bedPosition}</div>
+    </>
   );
 }
 
