35b1cc14aefaa5cbdb0638297e0ee522034f3dbc
brendancjz <brendancjz@gmail.com>
2023-10-31 12:06:49 +0800

    Add isArchived functionalitis into chat

--- src/models/chat.ts
+++ src/models/chat.ts
@@ -3,9 +3,10 @@ import { Message } from "./message";
 import { VirtualNurse } from "./virtualNurse";
 
 export type Chat = {
-    _id: string;
-    virtualNurse: VirtualNurse;
-    bedsideNurse: BedSideNurse;
-    latestMessage: string;
-    messages: Message[];
-  };
+  _id: string;
+  virtualNurse: VirtualNurse;
+  bedsideNurse: BedSideNurse;
+  latestMessage: string;
+  messages: Message[];
+  isArchived: boolean;
+};
