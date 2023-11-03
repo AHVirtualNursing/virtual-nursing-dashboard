67f3c6251fd05daa61e4357cd084a375fa64d2ea
wglee99 <littlereddot40@gmail.com>
2023-11-03 22:18:43 +0800

    feat: [AlertsSummary] add alerts summary for dashboard

--- src/components/AlertsSummary.tsx
+++ src/components/AlertsSummary.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+import { Alert } from "@/models/alert";
+import { fetchAllAlerts } from "@/pages/api/alerts_api";
+import React, { useEffect, useState } from "react";
 import { BarChart, Bar, ResponsiveContainer, XAxis, YAxis } from "recharts";
 
 const data = [
@@ -11,27 +13,44 @@ const data = [
 ];
 
 const AlertsSummary = () => {
+  const [alerts, setAlerts] = useState<Alert[]>();
+
+  useEffect(() => {
+    fetchAllAlerts().then((alerts) => {
+      setAlerts(alerts.data);
+    });
+  }, []);
+
+  const alertsData = [
+    {
+      name: "Page B",
+      open: alerts?.filter((alert) => alert.status === "open").length,
+      handling: alerts?.filter((alert) => alert.status === "handling").length,
+      completed: alerts?.filter((alert) => alert.status === "complete").length,
+    },
+  ];
+
   return (
     <div className="flex flex-col w-1/2 p-4 gap-y-3">
       <h3 className="text-left">Alerts Summary</h3>
       <div className="h-full flex gap-x-5">
         <div className="w-1/4 rounded-md bg-pink-200 flex items-center justify-center p-3">
-          <p>Total: 140</p>
+          <p>Total: {alerts && alerts.length}</p>
         </div>
         <div className="w-3/4">
           <ResponsiveContainer width="80%" height="100%">
             <BarChart
               width={150}
               height={40}
-              data={data}
+              data={alertsData}
               maxBarSize={30}
               layout="vertical"
             >
               <XAxis type="number" hide />
               <YAxis dataKey="name" type="category" hide />
               <Bar
-                dataKey="uv"
-                fill="#7F94DA"
+                dataKey="open"
+                fill="#FF7373"
                 background={{
                   radius: 20,
                   height: 15,
@@ -40,18 +59,18 @@ const AlertsSummary = () => {
                 label={{ fill: "black", fontSize: 10 }}
               />
               <Bar
-                dataKey="pv"
-                fill="#82ca9d"
-                background={{
-                  radius: 20,
-                  height: 15,
-                }}
-                radius={[10, 10, 10, 10]}
-                label={{ fill: "black", fontSize: 10 }}
-              />
-              <Bar
-                dataKey="amt"
+                dataKey="handling"
                 fill="#ffc658"
+                background={{
+                  radius: 20,
+                  height: 15,
+                }}
+                radius={[10, 10, 10, 10]}
+                label={{ fill: "black", fontSize: 10 }}
+              />
+              <Bar
+                dataKey="completed"
+                fill="#82ca9d"
                 background={{
                   radius: 20,
                   height: 15,
