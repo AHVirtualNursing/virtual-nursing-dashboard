7235cff595580ba8dfc410e4a30ccd82b6e404c3
wglee99 <littlereddot40@gmail.com>
2023-11-09 15:13:53 +0800

    style: alert details modal

--- src/components/AlertDetailsModal.tsx
+++ src/components/AlertDetailsModal.tsx
@@ -49,10 +49,49 @@ const AlertDetailsModal = ({
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
+          {/* <Typography id="modal-modal-title" variant="h6" component="h2">
             Alert Details
-          </Typography>
-          <div className="space-y-2">
+          </Typography> */}
+          <div className="space-y-6">
+            <div className="flex justify-between items-center font-bold text-lg">
+              <p>{patientName}</p>
+              <p
+                className={`${
+                  status === "open" ? "text-red-500" : "text-orange-500"
+                } uppercase`}
+              >
+                {status}
+              </p>
+            </div>
+            <p className="text-gray-600">{description}</p>
+            <p className="text-lg">Nurse: {handledBy ? handledBy : "-"}</p>
+            <p className="text-lg">
+              Time: {createdAt?.replace("T", " ").substring(0, 16)}
+            </p>
+            <div className="space-y-4 text-md">
+              <h4>Abnormal Vitals</h4>
+              {alertVitals &&
+                alertVitals.map((alert) => (
+                  <div className="flex gap-x-10 border-solid border-0 border-b-2 p-3">
+                    <p>{alert.vital}</p>
+                    <p className="font-bold">{alert.reading}</p>
+                  </div>
+                ))}
+            </div>
+            <button
+              className="float-right p-2 rounded-lg bg-blue-900 text-white"
+              onClick={handleClose}
+            >
+              Redelegate
+            </button>
+            <button
+              className="float-right p-2 mx-2 rounded-lg bg-white text-black"
+              onClick={handleClose}
+            >
+              Close
+            </button>
+          </div>
+          {/* <div className="space-y-2">
             <DetailBox title="Patient:" content={patientName} />
             <DetailBox title="Status:" content={status} />
             <DetailBox title="Description:" content={description} />
@@ -69,19 +108,7 @@ const AlertDetailsModal = ({
               title="Time:"
               content={createdAt?.replace("T", " ").substring(0, 16)}
             />
-          </div>
-          <button
-            className="float-right p-2 rounded-lg bg-blue-900 text-white"
-            onClick={handleClose}
-          >
-            Redelegate
-          </button>
-          <button
-            className="float-right p-2 mx-2 rounded-lg bg-white text-black"
-            onClick={handleClose}
-          >
-            Close
-          </button>
+          </div> */}
         </Box>
       </Modal>
     </div>
