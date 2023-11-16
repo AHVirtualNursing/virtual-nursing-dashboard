0a5c654f72c8587a3cefacbfee3a0408be72556e
wglee99 <littlereddot40@gmail.com>
2023-11-16 15:07:32 +0800

    feat: abstract out filter logic for all alerts and patient alerts pages

--- src/components/patientAlertTab/PatientAlerts.tsx
+++ src/components/patientAlertTab/PatientAlerts.tsx
@@ -46,8 +46,29 @@ const PatientAlerts = (patientProp: PatientProp) => {
     setShown(true);
   };
 
+  const filteredAlerts =
+    alerts &&
+    alerts
+      .sort((a, b) => +new Date(b.createdAt) - +new Date(a.createdAt))
+      .filter((alert) => {
+        const include =
+          alert.status.toLowerCase().includes(statusCriteria) &&
+          alert.alertType.toLowerCase().includes(alertTypeCriteria) &&
+          (vitalCriteria === ""
+            ? true
+            : alert.alertVitals.some((alertVital) =>
+                (alertVital as AlertVitals).vital
+                  .toLowerCase()
+                  .includes(vitalCriteria)
+              )) &&
+          (nurseSearch === ""
+            ? true
+            : alert.notes[0]?.addedBy.toLowerCase().includes(nurseSearch));
+        return include;
+      });
+
   return (
-    <div className="overflow-auto scrollbar bg-white rounded-lg shadow-lg p-4 h-auto max-h-[700px] space-y-3">
+    <>
       {shown && (
         <AlertDetailsModal
           pressed={shown}
@@ -55,147 +76,120 @@ const PatientAlerts = (patientProp: PatientProp) => {
           alert={selectedAlert}
         />
       )}
-      <table className="table-auto md:table-fixed border-collapse w-full">
-        <thead className="text-sm bg-slate-100">
-          {/* ------ column headers ------ */}
-          <tr>
-            <TableHeader title="Status" />
-            <TableHeader title="Alert Type" />
-            <TableHeader title="Vital Type" colspan={2} />
-            <TableHeader title="Description" />
-            <TableHeader title="Bedside Nurse" />
-            <TableHeader title="Time" />
-          </tr>
-        </thead>
+      <div className="overflow-auto scrollbar bg-white rounded-lg shadow-lg p-4 h-auto max-h-[700px] space-y-3">
+        <table className="table-auto md:table-fixed border-collapse w-full">
+          <thead className="text-sm bg-slate-100">
+            {/* ------ column headers ------ */}
+            <tr>
+              <TableHeader title="Status" />
+              <TableHeader title="Alert Type" />
+              <TableHeader title="Vital Type" colspan={2} />
+              <TableHeader title="Description" />
+              <TableHeader title="Bedside Nurse" />
+              <TableHeader title="Time" />
+            </tr>
+          </thead>
 
-        <tbody>
-          <tr id="subheaders">
-            <td id="status-filter">
-              {/* <select
-                name="status-select"
-                className="bg-white p-1 rounded-lg w-auto"
-                value={statusCriteria}
-                onChange={(e) => setStatusCriteria(e.target.value)}
-              >
-                <option value="">All</option>
-                <option value="open">Open</option>
-                <option value="handling">Handling</option>
-                <option value="complete">Complete</option>
-              </select> */}
-              <SelectFilter
-                name="statusSelect"
-                options={["all", "open", "handling", "complete"]}
-                changeSelectedOption={setStatusCriteria}
-                inTable={true}
-              />
-            </td>
-            <td id="alertType-filter">
-              <SelectFilter
-                name="alertTypeSelect"
-                options={["all", "vital", "smartbed"]}
-                changeSelectedOption={setAlertTypeCriteria}
-                inTable={true}
-              />
-            </td>
-            <td id="vital-filter">
-              <SelectFilter
-                name="vitalFilter"
-                options={[
-                  "all vitals",
-                  "heart rate",
-                  "blood pressure",
-                  "respiratory rate",
-                  "spo2",
-                  "temperature",
-                ]}
-                changeSelectedOption={setVitalCriteria}
-                inTable={true}
-              />
-            </td>
+          <tbody>
+            <tr id="subheaders">
+              <td id="status-filter">
+                <SelectFilter
+                  name="statusSelect"
+                  options={["all", "open", "handling", "complete"]}
+                  changeSelectedOption={setStatusCriteria}
+                  inTable={true}
+                />
+              </td>
+              <td id="alertType-filter">
+                <SelectFilter
+                  name="alertTypeSelect"
+                  options={["all", "vital", "smartbed"]}
+                  changeSelectedOption={setAlertTypeCriteria}
+                  inTable={true}
+                />
+              </td>
+              <td id="vital-filter">
+                <SelectFilter
+                  name="vitalFilter"
+                  options={[
+                    "all vitals",
+                    "heart rate",
+                    "blood pressure",
+                    "respiratory rate",
+                    "spo2",
+                    "temperature",
+                  ]}
+                  changeSelectedOption={setVitalCriteria}
+                  inTable={true}
+                />
+              </td>
 
-            <td id="vital-reading" className="text-sm underline">
-              Reading
-            </td>
-            <td>{""}</td>
+              <td id="vital-reading" className="text-sm underline">
+                Reading
+              </td>
+              <td>{""}</td>
 
-            <td id="nurse-filter">
-              <input
-                type="text"
-                className="placeholder:italic placeholder:text-slate-400 w-2/3 rounded-md placeholder:text-sm p-2 focus:outline-none focus:border-sky-500 border boder-slate-300"
-                placeholder="Search"
-                value={nurseSearch}
-                onChange={(e) => setNurseSearch(e.target.value)}
-              />
-            </td>
-            <td></td>
-          </tr>
+              <td id="nurse-filter">
+                <input
+                  type="text"
+                  className="placeholder:italic placeholder:text-slate-400 w-2/3 rounded-md placeholder:text-sm p-2 focus:outline-none focus:border-sky-500 border boder-slate-300"
+                  placeholder="Search"
+                  value={nurseSearch}
+                  onChange={(e) => setNurseSearch(e.target.value)}
+                />
+              </td>
+              <td></td>
+            </tr>
 
-          {alerts &&
-            alerts
-              .sort((a, b) => +new Date(b.createdAt) - +new Date(a.createdAt))
-              .filter((alert) =>
-                alert.status.toLowerCase().includes(statusCriteria)
-              )
-              .filter((alert) =>
-                alert.alertType.toLowerCase().includes(alertTypeCriteria)
-              )
-              .filter((alert) => {
-                return vitalCriteria === ""
-                  ? true
-                  : alert.alertVitals.some((alertVital) =>
-                      (alertVital as AlertVitals).vital
-                        .toLowerCase()
-                        .includes(vitalCriteria)
-                    );
-              })
-              .map((alert, index) => (
-                <tr key={index} onClick={() => handleViewAlertDetails(alert)}>
-                  <td
-                    className={`text-sm border-solid border-0 border-b border-slate-400`}
+            {filteredAlerts.map((alert, index) => (
+              <tr key={index} onClick={() => handleViewAlertDetails(alert)}>
+                <td
+                  className={`text-sm border-solid border-0 border-b border-slate-400`}
+                >
+                  <button
+                    className={`${
+                      alert.status === "open"
+                        ? "bg-red-400"
+                        : alert.status === "handling"
+                        ? "bg-orange-400"
+                        : "bg-green-400"
+                    } text-white py-1 px-3 rounded-lg w-auto uppercase pointer-events-none`}
                   >
-                    <button
-                      className={`${
-                        alert.status === "open"
-                          ? "bg-red-400"
-                          : alert.status === "handling"
-                          ? "bg-orange-400"
-                          : "bg-green-400"
-                      } text-white py-1 px-3 rounded-lg w-auto uppercase pointer-events-none`}
-                    >
-                      {alert.status}
-                    </button>
-                  </td>
-                  <AlertsTableRow id="alert-type" data={alert.alertType} />
-                  <AlertsTableRow
-                    id="abnormal-vital"
-                    data={alert.alertVitals.map(
-                      (value) => (value as AlertVitals).vital
-                    )}
-                  />
-                  <AlertsTableRow
-                    id="abnormal-vital-reading"
-                    data={alert.alertVitals.map(
-                      (value) => (value as AlertVitals).reading
-                    )}
-                  />
-                  <AlertsTableRow
-                    id="alert-description"
-                    data={alert.description}
-                  />
-                  <AlertsTableRow
-                    id="handling-nurse"
-                    width="1/12"
-                    data={alert.notes[0]?.addedBy}
-                  />
-                  <AlertsTableRow
-                    id="alert-datetime"
-                    data={alert.createdAt.replace("T", " ").substring(0, 16)}
-                  />
-                </tr>
-              ))}
-        </tbody>
-      </table>
-    </div>
+                    {alert.status}
+                  </button>
+                </td>
+                <AlertsTableRow id="alert-type" data={alert.alertType} />
+                <AlertsTableRow
+                  id="abnormal-vital"
+                  data={alert.alertVitals.map(
+                    (value) => (value as AlertVitals).vital
+                  )}
+                />
+                <AlertsTableRow
+                  id="abnormal-vital-reading"
+                  data={alert.alertVitals.map(
+                    (value) => (value as AlertVitals).reading
+                  )}
+                />
+                <AlertsTableRow
+                  id="alert-description"
+                  data={alert.description}
+                />
+                <AlertsTableRow
+                  id="handling-nurse"
+                  width="1/12"
+                  data={alert.notes[0]?.addedBy}
+                />
+                <AlertsTableRow
+                  id="alert-datetime"
+                  data={alert.createdAt.replace("T", " ").substring(0, 16)}
+                />
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 };
 
--- src/pages/alerts.tsx
+++ src/pages/alerts.tsx
@@ -7,7 +7,6 @@ import { useSession } from "next-auth/react";
 import { fetchWardsByVirtualNurse } from "./api/nurse_api";
 import { fetchAlertsByWardId } from "./api/wards_api";
 import { Ward } from "@/types/ward";
-import AlertsTable from "@/components/alerts/AlertsTable";
 import SelectFilter from "@/components/SelectFilter";
 import { SocketContext } from "./layout";
 
@@ -89,199 +88,200 @@ const Alerts = () => {
     setShown(true);
   };
 
+  const filteredAlerts =
+    alerts &&
+    alerts
+      .sort(
+        (a, b) => +new Date(b.alert.createdAt) - +new Date(a.alert.createdAt)
+      )
+      .filter((alertMapping) => {
+        const include =
+          alertMapping.patient.toLowerCase().includes(patientSearch) &&
+          alertMapping.alert.status.toLowerCase().includes(statusCriteria) &&
+          alertMapping.alert.alertType
+            .toLowerCase()
+            .includes(alertTypeCriteria) &&
+          (vitalCriteria === ""
+            ? true
+            : alertMapping.alert.alertVitals.some((alertVital) =>
+                (alertVital as AlertVitals).vital
+                  .toLowerCase()
+                  .includes(vitalCriteria)
+              )) &&
+          (nurseSearch === ""
+            ? true
+            : alertMapping.alert.notes[0]?.addedBy
+                .toLowerCase()
+                .includes(nurseSearch));
+        return include;
+      });
+
   return (
-    <div className="bg-slate-100 p-6 w-full">
-      <div>
-        <div className="flex flex-start p-2">
-          <h4>Alerts Overview</h4>
+    <>
+      {shown && (
+        <AlertDetailsModal
+          pressed={shown}
+          setShown={setShown}
+          alertPatientMapping={selectedAlert}
+        />
+      )}
+      <div className="bg-slate-100 p-6 w-full">
+        <div>
+          <div className="flex flex-start p-2">
+            <h4>Alerts Overview</h4>
+          </div>
         </div>
-      </div>
-      {/* <AlertsTable alerts={alerts} /> */}
-      <div className="overflow-auto scrollbar bg-white rounded-lg shadow-lg p-4 h-auto max-h-[720px] space-y-3">
-        {shown && (
-          <AlertDetailsModal
-            pressed={shown}
-            setShown={setShown}
-            alertPatientMapping={selectedAlert}
-          />
-        )}
-        <div className="gap-x-3 flex justify-start items-center">
-          <label
-            htmlFor="alertWardFilter"
-            className="text-sm font-medium text-gray-900"
-          >
-            Wards
-          </label>
-          <SelectFilter
-            name="alertWardFilter"
-            inTable={false}
-            changeSelectedOption={setSelectedWard}
-            options={["all"].concat(wards.map((ward) => ward.wardNum))}
-          />
-        </div>
+        <div className="overflow-auto scrollbar bg-white rounded-lg shadow-lg p-4 h-auto max-h-[720px] space-y-3">
+          <div className="gap-x-3 flex justify-start items-center">
+            <label
+              htmlFor="alertWardFilter"
+              className="text-sm font-medium text-gray-900"
+            >
+              Wards
+            </label>
+            <SelectFilter
+              name="alertWardFilter"
+              inTable={false}
+              changeSelectedOption={setSelectedWard}
+              options={["all"].concat(wards.map((ward) => ward.wardNum))}
+            />
+          </div>
 
-        <table className="table-auto md:table-fixed border-collapse w-full">
-          <thead className="text-sm bg-slate-100">
-            <tr>
-              <TableHeader title="Patient" />
-              <TableHeader title="Status" />
-              <TableHeader title="Alert Type" />
-              <TableHeader title="Vital Type" colspan={2} />
-              <TableHeader title="Description" />
-              <TableHeader title="Bedside Nurse" />
-              <TableHeader title="Time" />
-            </tr>
-          </thead>
+          <table className="table-auto md:table-fixed border-collapse w-full">
+            <thead className="text-sm bg-slate-100">
+              <tr>
+                <TableHeader title="Patient" />
+                <TableHeader title="Status" />
+                <TableHeader title="Alert Type" />
+                <TableHeader title="Vital Type" colspan={2} />
+                <TableHeader title="Description" />
+                <TableHeader title="Bedside Nurse" />
+                <TableHeader title="Time" />
+              </tr>
+            </thead>
 
-          <tbody>
-            <tr id="subheaders">
-              <td id="patient-filter">
-                <input
-                  type="text"
-                  className="placeholder:italic placeholder:text-slate-400 w-2/3 rounded-md placeholder:text-sm p-2 focus:outline-none focus:border-sky-500 border boder-slate-300"
-                  placeholder="Search"
-                  value={patientSearch}
-                  onChange={(e) => setPatientSearch(e.target.value)}
-                />
-              </td>
-              <td id="status-filter">
-                <SelectFilter
-                  name="statusSelect"
-                  options={["all", "open", "handling", "complete"]}
-                  changeSelectedOption={setStatusCriteria}
-                  inTable={true}
-                />
-              </td>
-              <td id="alertType-filter">
-                <SelectFilter
-                  name="alertTypeSelect"
-                  options={["all", "vital", "smartbed"]}
-                  changeSelectedOption={setAlertTypeCriteria}
-                  inTable={true}
-                />
-              </td>
-              <td id="vital-filter">
-                <SelectFilter
-                  name="vitalFilter"
-                  options={[
-                    "all vitals",
-                    "heart rate",
-                    "blood pressure",
-                    "respiratory rate",
-                    "spo2",
-                    "temperature",
-                  ]}
-                  changeSelectedOption={setVitalCriteria}
-                  inTable={true}
-                />
-              </td>
+            <tbody>
+              <tr id="subheaders">
+                <td id="patient-filter">
+                  <input
+                    type="text"
+                    className="placeholder:italic placeholder:text-slate-400 w-2/3 rounded-md placeholder:text-sm p-2 focus:outline-none focus:border-sky-500 border boder-slate-300"
+                    placeholder="Search"
+                    value={patientSearch}
+                    onChange={(e) => setPatientSearch(e.target.value)}
+                  />
+                </td>
+                <td id="status-filter">
+                  <SelectFilter
+                    name="statusSelect"
+                    options={["all", "open", "handling", "complete"]}
+                    changeSelectedOption={setStatusCriteria}
+                    inTable={true}
+                  />
+                </td>
+                <td id="alertType-filter">
+                  <SelectFilter
+                    name="alertTypeSelect"
+                    options={["all", "vital", "smartbed"]}
+                    changeSelectedOption={setAlertTypeCriteria}
+                    inTable={true}
+                  />
+                </td>
+                <td id="vital-filter">
+                  <SelectFilter
+                    name="vitalFilter"
+                    options={[
+                      "all vitals",
+                      "heart rate",
+                      "blood pressure",
+                      "respiratory rate",
+                      "spo2",
+                      "temperature",
+                    ]}
+                    changeSelectedOption={setVitalCriteria}
+                    inTable={true}
+                  />
+                </td>
 
-              <td id="vital-reading" className="text-sm underline">
-                Reading
-              </td>
-              <td>{""}</td>
+                <td id="vital-reading" className="text-sm underline">
+                  Reading
+                </td>
+                <td>{""}</td>
 
-              <td id="nurse-filter">
-                <input
-                  type="text"
-                  className="placeholder:italic placeholder:text-slate-400 w-2/3 rounded-md placeholder:text-sm p-2 focus:outline-none focus:border-sky-500 border boder-slate-300"
-                  placeholder="Search"
-                  value={nurseSearch}
-                  onChange={(e) => setNurseSearch(e.target.value)}
-                />
-              </td>
-              <td></td>
-            </tr>
+                <td id="nurse-filter">
+                  <input
+                    type="text"
+                    className="placeholder:italic placeholder:text-slate-400 w-2/3 rounded-md placeholder:text-sm p-2 focus:outline-none focus:border-sky-500 border boder-slate-300"
+                    placeholder="Search"
+                    value={nurseSearch}
+                    onChange={(e) => setNurseSearch(e.target.value)}
+                  />
+                </td>
+                <td></td>
+              </tr>
 
-            {alerts &&
-              alerts
-                .sort(
-                  (a, b) =>
-                    +new Date(b.alert.createdAt) - +new Date(a.alert.createdAt)
-                )
-                .filter((alertMapping) =>
-                  alertMapping.patient.toLowerCase().includes(patientSearch)
-                )
-                .filter((alertMapping) =>
-                  alertMapping.alert.status
-                    .toLowerCase()
-                    .includes(statusCriteria)
-                )
-                .filter((alertMapping) =>
-                  alertMapping.alert.alertType
-                    .toLowerCase()
-                    .includes(alertTypeCriteria)
-                )
-                .filter((alertMapping) => {
-                  return vitalCriteria === ""
-                    ? true
-                    : alertMapping.alert.alertVitals.some((alertVital) =>
-                        (alertVital as AlertVitals).vital
-                          .toLowerCase()
-                          .includes(vitalCriteria)
-                      );
-                })
-                .map((alertMapping, index) => (
-                  <tr
-                    key={index}
-                    onClick={() => handleViewAlertDetails(alertMapping)}
+              {filteredAlerts?.map((alertMapping, index) => (
+                <tr
+                  key={index}
+                  onClick={() => handleViewAlertDetails(alertMapping)}
+                >
+                  <AlertsTableRow
+                    id="patient-name"
+                    data={alertMapping.patient}
+                  />
+                  <td
+                    className={`text-sm border-solid border-0 border-b border-slate-400`}
                   >
-                    <AlertsTableRow
-                      id="patient-name"
-                      data={alertMapping.patient}
-                    />
-                    <td
-                      className={`text-sm border-solid border-0 border-b border-slate-400`}
+                    <button
+                      className={`${
+                        alertMapping.alert.status === "open"
+                          ? "bg-red-400"
+                          : alertMapping.alert.status === "handling"
+                          ? "bg-orange-400"
+                          : "bg-green-400"
+                      } text-white py-1 px-3 rounded-lg w-auto uppercase pointer-events-none`}
                     >
-                      <button
-                        className={`${
-                          alertMapping.alert.status === "open"
-                            ? "bg-red-400"
-                            : alertMapping.alert.status === "handling"
-                            ? "bg-orange-400"
-                            : "bg-green-400"
-                        } text-white py-1 px-3 rounded-lg w-auto uppercase pointer-events-none`}
-                      >
-                        {alertMapping.alert.status}
-                      </button>
-                    </td>
-                    <AlertsTableRow
-                      id="alert-type"
-                      data={alertMapping.alert.alertType}
-                    />
-                    <AlertsTableRow
-                      id="abnormal-vital"
-                      data={alertMapping.alert.alertVitals.map(
-                        (value) => (value as AlertVitals).vital
-                      )}
-                    />
-                    <AlertsTableRow
-                      id="abnormal-vital-reading"
-                      data={alertMapping.alert.alertVitals.map(
-                        (value) => (value as AlertVitals).reading
-                      )}
-                    />
-                    <AlertsTableRow
-                      id="alert-description"
-                      data={alertMapping.alert.description}
-                    />
-                    <AlertsTableRow
-                      id="handling-nurse"
-                      width="1/12"
-                      data={alertMapping.alert.notes[0]?.addedBy}
-                    />
-                    <AlertsTableRow
-                      id="alert-datetime"
-                      data={alertMapping.alert.createdAt
-                        .replace("T", " ")
-                        .substring(0, 16)}
-                    />
-                  </tr>
-                ))}
-          </tbody>
-        </table>
+                      {alertMapping.alert.status}
+                    </button>
+                  </td>
+                  <AlertsTableRow
+                    id="alert-type"
+                    data={alertMapping.alert.alertType}
+                  />
+                  <AlertsTableRow
+                    id="abnormal-vital"
+                    data={alertMapping.alert.alertVitals.map(
+                      (value) => (value as AlertVitals).vital
+                    )}
+                  />
+                  <AlertsTableRow
+                    id="abnormal-vital-reading"
+                    data={alertMapping.alert.alertVitals.map(
+                      (value) => (value as AlertVitals).reading
+                    )}
+                  />
+                  <AlertsTableRow
+                    id="alert-description"
+                    data={alertMapping.alert.description}
+                  />
+                  <AlertsTableRow
+                    id="handling-nurse"
+                    width="1/12"
+                    data={alertMapping.alert.notes[0]?.addedBy}
+                  />
+                  <AlertsTableRow
+                    id="alert-datetime"
+                    data={alertMapping.alert.createdAt
+                      .replace("T", " ")
+                      .substring(0, 16)}
+                  />
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
